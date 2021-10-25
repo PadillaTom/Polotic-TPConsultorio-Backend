c1f8fbf11b439b6466a4d85eb9519ac5ee43c185
Tomas Padilla <padillatomasagustin@gmail.com>
2021-10-25 21:32:20 +0200

    Architecture DTO Done

---
+++ src/main/java/com/padillatomas/consultorio/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.padillatomas.consultorio.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+	
+	// Instanciamos: SERVICE
+
+	
+	// *********************
+	// Metodos
+	// *********************
+	
+	// === POST ===
+	// === GET ===
+	// === PUT ===
+	// === DELETE ===
+
+
+}
--- src/main/java/com/padillatomas/consultorio/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.padillatomas.consultorio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.padillatomas.consultorio.entity.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-}
